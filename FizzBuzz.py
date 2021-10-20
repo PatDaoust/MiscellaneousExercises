32dbe9c33416a53c9f4fef4eaf151b496aef9854
Cataluna <catalunalilith2680@gmail.com>
2021-10-20 11:57:14 -0400

    message

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 15:10:13 2021
+
+@author: catal
+"""
+
+
+# rules:
+#     multiples of 3 = fizz
+#     multiples of 5 = buzz
+#     multiple of 3 and 5 = fizzbuzz
