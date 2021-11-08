9ccf15f9ff1aa398312485a0f76c0e7a62d33192
Cataluna <catalunalilith2680@gmail.com>
2021-11-08 11:07:02 -0500

    setup generateSentences()

--- ProgrammingHero exercises.py
+++ ProgrammingHero exercises.py
@@ -954,10 +954,50 @@ def passwordWithRequirements(lenght=12):
 # print(passwordWithRequirements(2500))
 
 
+def get_permutations(sequence):
+    '''
+    Assumes sequence is an arbitrary string and non-empty
+    Returns: a list of strings, all the permutations of sequence
+    Example: get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+    note: do not depend on order of the output list
+    '''
+    permutations_of_sequence = []
+    permutations_of_cutdown_sequence = []
+    # define recursive behaviour
+    # for sequence of len(n), case is sequence[0] and sequence[1:n]
+    if len(sequence) > 1:
+        first_char_sequence = sequence[0]
+        cutdown_sequence = sequence[1:]
+        # find recursions of cutdown_sequence
+        if len(cutdown_sequence) > 1:
+            permutations_of_cutdown_sequence = get_permutations(sequence[1:])
+        # base case
+        # add first_char_sequence to all recursions of cutdown_sequence
+        for i in range(len(sequence)):
+            holding_string = cutdown_sequence[:i] + first_char_sequence\
+                + cutdown_sequence[i:]
+            permutations_of_sequence += [holding_string]
+        # loop over each item in permutations_of_cutdown_sequence
+        if len(permutations_of_cutdown_sequence) > 1:
+            for ele in range(len(permutations_of_cutdown_sequence)):
+                new_cutdown_sequence = permutations_of_cutdown_sequence[ele]
+                for i in range(len(sequence)):
+                    holding_string = new_cutdown_sequence[:i] +\
+                        first_char_sequence + new_cutdown_sequence[i:]
+                    permutations_of_sequence += [holding_string]
+    # remove duplicates from permutations_of_sequence
+    return list(dict.fromkeys(permutations_of_sequence))
 
-def permutations():
+
+def generateSentences(words_list_list):
+    """assumes words_list_list is a list of lists of strings
+    returns a string, representing a sentence,
+    made of 1 string from each sub list of words_list_list
+    """
     pass
 
 
-def generateSentences():
-    pass
+# TODO make 3 word lists
+# TODO make words_list_list
+# print(generateSentences(words_list_list))
