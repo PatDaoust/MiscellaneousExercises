9583391754fb107e0875d2215f735b91a380cad2
Cataluna <catalunalilith2680@gmail.com>
2021-11-14 13:11:40 -0500

    setup learnbyexample exercise Q1a

---
+++ learnbyexamplePythonBasics.py
@@ -0,0 +1,20 @@
+"""
+many thanks to github.com/learnbyexample for these exercises
+full exercises at https://github.com/learnbyexample/Python_Basics/blob/master/Exercises.md
+"""
+
+"""
+Q1a) Ask user information, for ex: name, department, college etc and display them using print function
+"""
+
+# Sample of how program might ask user input and display output afterwards
+$ ./usr_ip.py 
+Please provide the following details
+Enter your name: learnbyexample
+Enter your department: ECE
+Enter your college: PSG Tech
+
+------------------------------------
+Name       : learnbyexample
+Department : ECE
+College    : PSG Tech
