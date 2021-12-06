30fe9939aa7e87c7048515fe117c681e584f57ac
Cataluna <catalunalilith2680@gmail.com>
2021-12-06 18:16:00 -0500

    implement all format exerciese

--- HolyPythonFormat.py
+++ HolyPythonFormat.py
@@ -47,3 +47,48 @@ def print123():
 
 
 # print123()
+
+"""Fill in the values for months, weeks and days accordingly."""
+
+
+def monthsWeeksDays():
+    """print 'One year has 12 months, 52 weeks and 365 days.' """
+    str = "One year has {} months, {} weeks and {} days.".format(12, 52, 365)
+    print(str)
+
+
+# monthsWeeksDays()
+
+
+"""
+You can also use indexing inside your curly brackets so that you will decide
+the position instead of a default order.
+
+By filling inside the curly brackets,
+make sure each bracket is matched to the appropriate value.
+"""
+
+
+def monthsWeeksDaysv2():
+    """print 'One year has 12 months, 52 weeks and 365 days.' """
+    str = "One year has {2} months, {0} weeks and {1} days.".format(52, 365, 12)
+    print(str)
+
+
+# monthsWeeksDaysv2()
+
+"""Fill inside .format()'s parenthesis so that it matches the correct values."""
+
+
+def scores():
+    John = 75
+    Ann = 80
+    Ally = 60
+    str1 = "Scores were as following: John:{}, Ann:{}, Ally:{}"
+
+    # Type your code here.
+    str2 = str1.format(John, Ann, Ally)
+    print(str2)
+
+
+scores()
