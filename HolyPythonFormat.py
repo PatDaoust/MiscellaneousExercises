722b0932e272d45c57b87faab6815101f198f450
Cataluna <catalunalilith2680@gmail.com>
2021-12-05 19:26:19 -0500

    implement first formatting exercise

--- HolyPythonFormat.py
+++ HolyPythonFormat.py
@@ -5,3 +5,20 @@ Created on Sun Dec  5 19:14:22 2021
 @author: catal
 """
 
+"""please see https://holypython.com/ for original exercises"""
+
+
+"""
+Place a curly bracket "{}" in the appropriate place
+and fill inside .format()'s parenthesis so that "Hello!, Name" is printed.
+"""
+
+
+def printGreeting():
+    """takes user input and prints a greeting"""
+    name = input("Please enter your name: ")
+    str = "Hello!, {}".format(name)
+    print(str)
+
+
+# printGreeting()
