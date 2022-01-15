8a2c2e0f07791b5f1fd7d10b30f629a2ac6ef4eb
Cataluna <catalunalilith2680@gmail.com>
2022-01-15 12:13:15 -0500

    implement exercise 16-f

--- HolyPythonFormat.py
+++ HolyPythonFormat.py
@@ -1203,3 +1203,21 @@ if the square is greater than 50."""
 
 lst60 = [x**2 for x in lst59 if x**2 > 50]
 # print(lst60)
+
+
+"""Exercise 16-f
+Given dictionary is consisted of vehicles and their weights in kilograms.
+Contruct a list of the names of vehicles with weight below 5000 kilograms.
+In the same list comprehension make the key names all upper case."""
+
+dict2 = {"Sedan": 1500,
+         "SUV": 2000,
+         "Pickup": 2500,
+         "Minivan": 1600,
+         "Van": 2400,
+         "Semi": 13600,
+         "Bicycle": 7,
+         "Motorcycle": 110}
+
+lst61 = [x.upper() for x in dict2 if dict2[x] < 5000]
+# print(lst61)
