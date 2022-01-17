4ad4b9f2db94129311d2f8077d1f96fa100f5387
Cataluna <catalunalilith2680@gmail.com>
2022-01-17 13:23:06 -0500

    clean up formating to match PEP8 guidelines

--- regexExercisesW3resource.py
+++ regexExercisesW3resource.py
@@ -11,94 +11,103 @@ solutions by Pat Daoust
 """
 
 """
-1. Write a Python program to check that a string contains only a certain set of characters (in this case a-z, A-Z and 0-9). Go to the editor
+1. Write a Python program to check that a string contains only a certain set of characters
+(in this case a-z, A-Z and 0-9).
 """
+
+"""
+2. Write a Python program that matches a string that has an a followed by zero or more b's
 """
 
-2. Write a Python program that matches a string that has an a followed by zero or more b's. Go to the editor
 
-Click me to see the solution
+"""
+3. Write a Python program that matches a string that has an a followed by one or more b's.
+"""
 
-3. Write a Python program that matches a string that has an a followed by one or more b's. Go to the editor
+"""
+4. Write a Python program that matches a string that has an a followed by zero or one 'b'.
+"""
 
-Click me to see the solution
+"""
+5. Write a Python program that matches a string that has an a followed by three 'b'.
+"""
 
-4. Write a Python program that matches a string that has an a followed by zero or one 'b'. Go to the editor
+"""
+6. Write a Python program that matches a string that has an a followed by two to three 'b'
+"""
 
-Click me to see the solution
+"""
+7. Write a Python program to find sequences of lowercase letters joined with a underscore.
+"""
 
-5. Write a Python program that matches a string that has an a followed by three 'b'. Go to the editor
+"""
+8. Write a Python program to find the sequences of one upper case letter
+followed by lower case letters.
+"""
 
-Click me to see the solution
+"""
+9. Write a Python program that matches a string that has an 'a' followed by anything,
+ending in 'b'.
+"""
 
-6. Write a Python program that matches a string that has an a followed by two to three 'b'. Go to the editor
+"""
+10. Write a Python program that matches a word at the beginning of a string.
+"""
 
-Click me to see the solution
+"""
+11. Write a Python program that matches a word at the end of string,
+with optional punctuation.
+"""
 
-7. Write a Python program to find sequences of lowercase letters joined with a underscore. Go to the editor
+"""
+12. Write a Python program that matches a word containing 'z'.
+"""
 
-Click me to see the solution
+"""
+13. Write a Python program that matches a word containing 'z',
+not at the start or end of the word.
+"""
 
-8. Write a Python program to find the sequences of one upper case letter followed by lower case letters. Go to the editor
+"""
+14. Write a Python program to match a string that contains only
+upper and lowercase letters, numbers, and underscores.
+"""
 
-Click me to see the solution
+"""
+15. Write a Python program where a string will start with a specific number.
+"""
 
-9. Write a Python program that matches a string that has an 'a' followed by anything, ending in 'b'. Go to the editor
+"""
+16. Write a Python program to remove leading zeros from an IP address.
+"""
 
-Click me to see the solution
+"""
+17. Write a Python program to check for a number at the end of a string.
+"""
 
-10. Write a Python program that matches a word at the beginning of a string. Go to the editor
-
-Click me to see the solution
-
-11. Write a Python program that matches a word at the end of string, with optional punctuation. Go to the editor
-
-Click me to see the solution
-
-12. Write a Python program that matches a word containing 'z'. Go to the editor
-
-Click me to see the solution
-
-13. Write a Python program that matches a word containing 'z', not at the start or end of the word. Go to the editor
-
-Click me to see the solution
-
-14. Write a Python program to match a string that contains only upper and lowercase letters, numbers, and underscores. Go to the editor
-
-Click me to see the solution
-
-15. Write a Python program where a string will start with a specific number. Go to the editor
-
-Click me to see the solution
-
-16. Write a Python program to remove leading zeros from an IP address. Go to the editor
-
-Click me to see the solution
-
-17. Write a Python program to check for a number at the end of a string. Go to the editor
-
-Click me to see the solution
-
-18. Write a Python program to search the numbers (0-9) of length between 1 to 3 in a given string. Go to the editor
+"""
+18. Write a Python program to search the numbers (0-9) of length between 1 to 3
+in a given string.
 
 "Exercises number 1, 12, 13, and 345 are important"
+"""
 
-Click me to see the solution
-
-19. Write a Python program to search some literals strings in a string. Go to the editor
+"""
+19. Write a Python program to search some literals strings in a string.
 Sample text : 'The quick brown fox jumps over the lazy dog.'
 Searched words : 'fox', 'dog', 'horse'
+"""
 
-Click me to see the solution
-
-20. Write a Python program to search a literals string in a string and also find the location within the original string where the pattern occurs. Go to the editor
+"""
+20. Write a Python program to search a literals string in a string
+and also find the location within the original string where the pattern occurs.
 
 Sample text : 'The quick brown fox jumps over the lazy dog.'
 Searched words : 'fox'
+"""
 
-Click me to see the solution
-
-21. Write a Python program to find the substrings within a string. Go to the editor
+"""
+21. Write a Python program to find the substrings within a string.
 
 Sample text :
 
@@ -109,169 +118,191 @@ Pattern :
 'exercises'
 
 Note: There are two instances of exercises in the input string.
+"""
 
-Click me to see the solution
+"""
+22. Write a Python program to find the occurrence and position of
+the substrings within a string.
+"""
 
-22. Write a Python program to find the occurrence and position of the substrings within a string. Go to the editor
+"""
+23. Write a Python program to replace whitespaces with an underscore and vice versa.
+"""
 
-Click me to see the solution
+"""
+24. Write a Python program to extract year, month and date from an url.
+"""
 
-23. Write a Python program to replace whitespaces with an underscore and vice versa. Go to the editor
+"""
+25. Write a Python program to convert a date of yyyy-mm-dd format to dd-mm-yyyy format.
+"""
 
-Click me to see the solution
+"""
+26. Write a Python program to match if two words from a list of words
+starting with letter 'P'.
+"""
 
-24. Write a Python program to extract year, month and date from an url. Go to the editor
+"""
+27. Write a Python program to separate and print the numbers of a given string.
+"""
 
-Click me to see the solution
+"""
+28. Write a Python program to find all words starting with 'a' or 'e' in a given string.
+"""
 
-25. Write a Python program to convert a date of yyyy-mm-dd format to dd-mm-yyyy format. Go to the editor
 
-Click me to see the solution
+"""
+29. Write a Python program to separate and print the numbers and their position
+of a given string.
+"""
 
-26. Write a Python program to match if two words from a list of words starting with letter 'P'. Go to the editor
+"""
+30. Write a Python program to abbreviate 'Road' as 'Rd.' in a given string.
+"""
 
-Click me to see the solution
+"""
+31. Write a Python program to replace all occurrences of space, comma, or dot with a colon
+"""
 
-27. Write a Python program to separate and print the numbers of a given string. Go to the editor
+"""
+32. Write a Python program to replace maximum 2 occurrences of
+space, comma, or dot with a colon.
+"""
 
-Click me to see the solution
+"""
+33. Write a Python program to find all five characters long word in a string.
+"""
 
-28. Write a Python program to find all words starting with 'a' or 'e' in a given string. Go to the editor
+"""
+34. Write a Python program to find all three, four, five characters long words in a string
+"""
 
-Click me to see the solution
+"""
+35. Write a Python program to find all words which are at least 4 characters long
+in a string.
+"""
 
-29. Write a Python program to separate and print the numbers and their position of a given string. Go to the editor
+"""
+36. Write a python program to convert camel case string to snake case string.
+"""
 
-Click me to see the solution
+"""
+37. Write a python program to convert snake case string to camel case string.
+"""
 
-30. Write a Python program to abbreviate 'Road' as 'Rd.' in a given string. Go to the editor
+"""
+38. Write a Python program to extract values between quotation marks of a string.
+"""
 
-Click me to see the solution
 
-31. Write a Python program to replace all occurrences of space, comma, or dot with a colon. Go to the editor
+"""
+39. Write a Python program to remove multiple spaces in a string.
+"""
 
-Click me to see the solution
+"""
+40. Write a Python program to remove all whitespaces from a string.
+"""
 
-32. Write a Python program to replace maximum 2 occurrences of space, comma, or dot with a colon. Go to the editor
+"""
+41. Write a Python program to remove everything except alphanumeric characters
+from a string.
+"""
 
-Click me to see the solution
+"""
+42. Write a Python program to find urls in a string.
+"""
 
-33. Write a Python program to find all five characters long word in a string. Go to the editor
+"""
+43. Write a Python program to split a string at uppercase letters.
+"""
 
-Click me to see the solution
+"""
+44. Write a Python program to do a case-insensitive string replacement.
+"""
 
-34. Write a Python program to find all three, four, five characters long words in a string. Go to the editor
+"""
+45. Write a Python program to remove the ANSI escape sequences from a string.
+"""
 
-Click me to see the solution
-
-35. Write a Python program to find all words which are at least 4 characters long in a string. Go to the editor
-
-Click me to see the solution
-
-36. Write a python program to convert camel case string to snake case string. Go to the editor
-
-Click me to see the solution
-
-37. Write a python program to convert snake case string to camel case string. Go to the editor
-
-Click me to see the solution
-
-38. Write a Python program to extract values between quotation marks of a string. Go to the editor
-
-Click me to see the solution
-
-39. Write a Python program to remove multiple spaces in a string. Go to the editor
-
-Click me to see the solution
-
-40. Write a Python program to remove all whitespaces from a string. Go to the editor
-
-Click me to see the solution
-
-41. Write a Python program to remove everything except alphanumeric characters from a string. Go to the editor
-
-Click me to see the solution
-
-42. Write a Python program to find urls in a string. Go to the editor
-
-Click me to see the solution
-
-43. Write a Python program to split a string at uppercase letters. Go to the editor
-
-Click me to see the solution
-
-44. Write a Python program to do a case-insensitive string replacement. Go to the editor
-
-Click me to see the solution
-
-45. Write a Python program to remove the ANSI escape sequences from a string. Go to the editor
-
-Click me to see the solution
-
-46. Write a Python program to find all adverbs and their positions in a given sentence. Go to the editor
+"""
+46. Write a Python program to find all adverbs and their positions in a given sentence.
 
 Sample text : "Clearly, he has no excuse for such behavior."
+"""
 
-Click me to see the solution
+"""
+47. Write a Python program to split a string with multiple delimiters.
 
-47. Write a Python program to split a string with multiple delimiters. Go to the editor
+Note : A delimiter is a sequence of one or more characters used to specify
+the boundary between separate, independent regions in plain text or other data streams.
+An example of a delimiter is the comma character,
+which acts as a field delimiter in a sequence of comma-separated values.
+"""
 
-Note : A delimiter is a sequence of one or more characters used to specify the boundary between separate, independent regions in plain text or other data streams. An example of a delimiter is the comma character, which acts as a field delimiter in a sequence of comma-separated values.
+"""
+48. Write a Python program to check a decimal with a precision of 2.
+"""
 
-Click me to see the solution
+"""
+49. Write a Python program to remove words from a string of length
+between 1 and a given number.
+"""
 
-48. Write a Python program to check a decimal with a precision of 2. Go to the editor
-
-Click me to see the solution
-
-49. Write a Python program to remove words from a string of length between 1 and a given number. Go to the editor
-
-Click me to see the solution
-
-50. Write a Python program to remove the parenthesis area in a string. Go to the editor
+"""
+50. Write a Python program to remove the parenthesis area in a string.
 Sample data : ["example (.com)", "w3resource", "github (.com)", "stackoverflow (.com)"]
 Expected Output:
 example
 w3resource
 github
 stackoverflow
-Click me to see the solution
+"""
 
-51. Write a Python program to insert spaces between words starting with capital letters. Go to the editor
-Click me to see the solution
+"""
+51. Write a Python program to insert spaces between words starting with capital letters.
+"""
 
-52. Write a Python program that reads a given expression and evaluates it. Go to the editor
+"""
+52. Write a Python program that reads a given expression and evaluates it.
 Terms and conditions:
-The expression consists of numerical values, operators and parentheses, and the ends with '='.
-The operators includes +, -, *, / where, represents, addition, subtraction, multiplication and division.
+The expression consists of numerical values, operators and parentheses, and ends with '='
+The operators includes +, -, *, / where, represents, addition, subtraction,
+multiplication and division.
 When two operators have the same precedence, they are applied to left to right.
 You may assume that there is no division by zero.
-All calculation is performed as integers, and after the decimal point should be truncated Length of the expression will not exceed 100.
+All calculation is performed as integers, and after the decimal point should be truncated
+Length of the expression will not exceed 100.
 -1 ? 10 9 = intermediate results of computation = 10 9
-Click me to see the solution
+"""
 
-53. Write a Python program to remove lowercase substrings from a given string. Go to the editor
-Click me to see the solution
+"""
+53. Write a Python program to remove lowercase substrings from a given string.
+"""
 
-54. Write a Python program to concatenate the consecutive numbers in a given string. Go to the editor
+"""
+54. Write a Python program to concatenate the consecutive numbers in a given string.
 Original string:
-Enter at 1 20 Kearny Street. The security desk can direct you to floor 1 6. Please have your identification ready.
+Enter at 1 20 Kearny Street. The security desk can direct you to floor 1 6.
+Please have your identification ready.
 After concatenating the consecutive numbers in the said string:
-Enter at 120 Kearny Street. The security desk can direct you to floor 16. Please have your identification ready.
-Click me to see the solution
+Enter at 120 Kearny Street. The security desk can direct you to floor 16.
+Please have your identification ready.
+"""
 
-55. Write a Python program to convert a given string to snake case. Go to the editor
+"""
+55. Write a Python program to convert a given string to snake case.
 Sample Output:
 java-script
 gd-script
 btw...-what-*-do*-you-call-that-naming-style?-snake-case?
-Click me to see the solution
+"""
 
-56. Write a Python program that takes any number of iterable objects or objects with a length property and returns the longest one.Go to the editor
+"""
+56. Write a Python program that takes any number of iterable objects or objects
+with a length property and returns the longest one.
 Sample Output:
 Orange
 [1, 2, 3, 4, 5]
 Java
 Python
-Click me to see the solution
 """
