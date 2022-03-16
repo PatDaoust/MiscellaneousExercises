49310ede2939262b8f3f25af9b150e1f5531ca67
Cataluna <catalunalilith2680@gmail.com>
2022-03-16 18:48:23 -0400

    implement basic gameplay

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -4,6 +4,7 @@ Created on Wed Mar 16 14:46:43 2022
 
 @author: catal
 """
+import random
 """
 guess the number game
 goal = commande line gameplay
@@ -13,8 +14,7 @@ c: ask user if want to play
     else: goodbye, close program
 guess_ count = 1
 c: ask user for desired range
-    option: enter ints
-    option: easy 1-10, medium 1-100, hard -1000-1000
+=    option: easy 1-10, hard -1000-1000
 c : select random number in range
 c: ask user to guess
 u: submit guess
@@ -28,6 +28,9 @@ c: if guess = num:
         your guess is too low
     if guess > num:
         your guess is too high
+        
+    option: track guesses that are too high and two low, display
+    display last guess
 """
 
 
@@ -37,7 +40,7 @@ def main():
 I'll pick a number, and you have to guess it!\n\
 If you want to play, type 'yes'\n")
     # initialize gameplay
-    if play_game == "Yes" or play_game == "yes":
+    if play_game.lower == "yes":
         play_game = True
     if play_game is not True:
         print("Okay. Let's play another time!")
@@ -46,21 +49,47 @@ If you want to play, type 'yes'\n")
     mode = None
     easy_valid = ['easy', 'easy mode']
     hard_valid = ['hard', 'hard mode']
-    if mode is None:
+    while mode is None:
         mode = input("Okay! Would you like to play in easy mode or hard mode?\n")
         mode = mode.lower()
         if mode in easy_valid:
             mode = "easy"
             print("you've chosen easy mode. I'll pick a random number between 1 and 100")
+            random_num = random.randint(1, 100)
         elif mode in hard_valid:
             mode = "hard"
             print("you've chosen hard mode. I'll pick a random number between -1000 and 1000")
+            random_num = random.randint(-1000, 1000)
         else:
             mode = None
             print("I'm sorry, I didn't understand that. \n\
-Please type 'easy' for easy mode, or 'hard' for hard mode")
+Please type 'easy' for easy mode, or 'hard' for hard mode\n")
     # guessing loop
     guesses_count = 0
+    guess = None
+    while guess != random_num:
+        # take guess
+        while guess is None:
+            guess = input("Please type a number\n")
+            try:
+                guess = int(guess)
+            except ValueError:
+                print("I'm sorry, I didn't understand that. \nPlease type a number\n")
+        # evaluate guess
+        if guess > random_num:
+            print("Nope, your guess of " + str(guess) + " was too high")
+            guess = None
+            guesses_count += 1
+        elif guess < random_num:
+            print("Nope, your guess of " + str(guess) + " was too low")
+        else:
+            # TODO win
+            print("you win!")
+            # how many guesses
+            break
+        guess = None
+        guesses_count += 1
+
 
 if __name__ == "__main__":
     main()
