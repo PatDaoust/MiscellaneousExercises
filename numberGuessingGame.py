9ce1290fdc5d5cf0e1acabdb1b4a06107141fd57
Cataluna <catalunalilith2680@gmail.com>
2022-03-16 14:58:43 -0400

    write project outline

---
+++ numberGuessingGame.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 14:46:43 2022
+
+@author: catal
+"""
+"""
+guess the number game
+goal = commande line gameplay
+
+c: ask user if want to play
+    if true: start game
+    else: goodbye, close program
+guess_ count = 1
+c: ask user for desired range 
+    option: enter ints
+    option: easy 1-10, medium 1-100, hard -1000-1000
+c : select random number in range
+c: ask user to guess
+u: submit guess
+    guess_count += 1
+c: if guess = num:
+    congrats, you got it in guess_count guesses
+        optional: sub program to give random congrats message
+    do you want to play anoyher round?
+        if true: restart variables
+    if guess < num:
+        your guess is too low
+    if guess > num:
+        your guess is too high
+"""
